34c67b1af4d38b1ffb4e3c7780b4bb566e19cdbb
Tacey Wong <Tacey_Wong@163.com>
2014-10-12 15:14:55 +0800

    完善cp1

--- cp1.c
+++ cp1.c
@@ -10,6 +10,7 @@
 
 #define BUFFERSIZE 4096
 #define COPYMODE  0644
+void oops(char *,char *);
 
 int main(int argc,char *argv[])
 {
@@ -18,14 +19,28 @@ int main(int argc,char *argv[])
 	
 	if(argc != 3)
 	{
-		fprintf(stderr,"usage:%s source destination\n",*argv)
+		fprintf(stderr,"usage:%s source destination\n",*argv);
 		exit(1);
 	}
 	
-	if((in_fd = open(av[1],O_RDONLY)) == -1)
+	if((in_fd = open(argv[1],O_RDONLY)) == -1)
 		oops("failed to open ",argv[1]);
 	if((out_fd = creat(argv[2],COPYMODE)) == -1)
 		oops("failed to open",argv[2]);
 	while((n_chars = read(in_fd,buf,BUFFERSIZE))>0)
-		if(n_chars == -1)
+		if(write( out_fd,buf,n_chars)!= n_chars)
+			oops(" Write error to ",argv[2]);
+	if( n_chars == -1)
+		oops("Read error from ",argv[1]);
+	
+	if (close(in_fd) == -1 || close(out_fd) == -1)
+		oops("Erroe closing files","");
+		
+}
+
+void oops(char *s1 , char *s2)
+{
+	fprintf(stderr,"Erroe: %s",s1);
+	perror(s2);
+	exit(1);
 }
